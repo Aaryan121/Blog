486f958148cb1eb9197007956dd606da3a148943
Aaryan121 <aaryan.cs01@gmail.com>
2022-11-11 15:29:32 +0545

    connected to database and register api implemented

--- api/index.js
+++ api/index.js
@@ -2,17 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
-
+const authRoute =  require("./routes/auth");
 
 dotenv.config()
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL,{})
 .then(console.log("connected to mongo"))
 .catch((err)=>console.log(err))
 
-app.use("/",(req,res)=>{
-    console.log("hey this is main url")
-})
+app.use("/api/auth",authRoute)
 
 
 app.listen("8000", ()=>{
